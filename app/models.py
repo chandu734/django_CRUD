2f279ef61e147f0f0f8a0c6734e4e5ab89d2991a
chandu734 <chandra01rout@gmail.com>
2024-06-28 15:45:54 +0530

    Fix Django module import issue

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class Employee(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
-    salary = models.FloatField( )
+    salary = models.FloatField()
     designation = models.CharField(max_length=200)
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return self.name
+
     
 
     
